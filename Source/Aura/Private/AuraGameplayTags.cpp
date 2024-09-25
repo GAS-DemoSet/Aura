dce711dcd4ca427ac88b3de46a047b0bcec0e72a
WIN-ST5EU1T38E7\Administrator <2649340476@qq.com>
2024-09-25 11:14:39 +0800

    feat: 9.11 Cpp中注册GameplayTag

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,85 @@
+﻿#include "AuraGameplayTags.h"
+
+#include "GameplayTagsManager.h"
+
+TSharedPtr<FAuraGameplayTags> FAuraGameplayTags::GameplayTags = nullptr;
+
+TSharedPtr<FAuraGameplayTags> FAuraGameplayTags::Get()
+{
+	if (!GameplayTags)
+	{
+		GameplayTags = MakeShareable(new(FAuraGameplayTags));
+	}
+	checkf(GameplayTags, TEXT("Error:: FAuraGameplayTags::Get() GameplayTags is null!!!"));
+	return GameplayTags;
+}
+
+void FAuraGameplayTags::Clear()
+{
+	if (GameplayTags)
+	{
+		GameplayTags->UnInitializeNativeGameplayTags();
+		GameplayTags.Reset();
+	}
+	GameplayTags = nullptr;
+}
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	/*
+	 * Secondary Attributes
+	 */
+	Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.Armor"),
+		FString("Reduces damage taken, improves Block Chance")
+		);
+
+	Attributes_Secondary_ArmorPenetration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.ArmorPenetration"),
+		FString("Ignores Percentage of enemy Armor, increases Critical Hit Chance")
+		);
+
+	Attributes_Secondary_BlockChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.BlockChance"),
+		FString("Chance to cut incoming damage in half")
+		);
+
+	Attributes_Secondary_CriticalHitChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.CriticalHitChance"),
+		FString("Chance to double damage plus critical hit bonus")
+		);
+
+	Attributes_Secondary_CriticalHitDamage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.CriticalHitDamage"),
+		FString("Bonus damage added when a critical hit is scored")
+		);
+
+	Attributes_Secondary_CriticalHitResistance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.CriticalHitResistance"),
+		FString("Reduces Critical Hit Chance of attacking enemies")
+		);
+
+	Attributes_Secondary_HealthRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.HealthRegeneration"),
+		FString("Amount of Health regenerated every 1 second")
+		);
+
+	Attributes_Secondary_ManaRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.ManaRegeneration"),
+		FString("Amount of Mana regenerated every 1 second")
+		);
+
+	Attributes_Secondary_MaxHealth = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.MaxHealth"),
+		FString("Maximum amount of Health obtainable")
+		);
+
+	Attributes_Secondary_MaxMana = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.MaxMana"),
+		FString("Maximum amount of Mana obtainable")
+		);
+}
+
+void FAuraGameplayTags::UnInitializeNativeGameplayTags()
+{
+}
---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+struct FAuraGameplayTags
+{
+public:
+	static TSharedPtr<FAuraGameplayTags> Get();
+	static void Clear();
+	
+	void InitializeNativeGameplayTags();
+	void UnInitializeNativeGameplayTags();
+
+
+	/*
+	 * Secondary Attributes
+	 */
+	FGameplayTag Attributes_Secondary_Armor;
+	FGameplayTag Attributes_Secondary_ArmorPenetration;
+	FGameplayTag Attributes_Secondary_BlockChance;
+	FGameplayTag Attributes_Secondary_CriticalHitChance;
+	FGameplayTag Attributes_Secondary_CriticalHitDamage;
+	FGameplayTag Attributes_Secondary_CriticalHitResistance;
+	FGameplayTag Attributes_Secondary_HealthRegeneration;
+	FGameplayTag Attributes_Secondary_ManaRegeneration;
+	FGameplayTag Attributes_Secondary_MaxHealth;
+	FGameplayTag Attributes_Secondary_MaxMana;
+
+
+private:
+	static TSharedPtr<FAuraGameplayTags> GameplayTags;
+};
