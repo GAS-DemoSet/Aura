3716accbed6afeddd3850bed659631fa03e6c107
WIN-ST5EU1T38E7\Administrator <2649340476@qq.com>
2024-09-26 16:38:05 +0800

    feat: 10.5 创建输入组件

---
+++ Source/Aura/Public/Input/AuraInputComponent.h
@@ -0,0 +1,48 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraInputConfig.h"
+#include "EnhancedInputComponent.h"
+#include "AuraInputComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class AURA_API UAuraInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+
+public:
+	UAuraInputComponent();
+
+	/**  */
+	template<class UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
+	void BindAbilityActions(const UAuraInputConfig* InputConfig, UserClass* Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HeldFunc)
+	{
+		checkf(InputConfig, TEXT("error: BindAbilityActions:: InputConfig null!!!"));
+
+		for (auto& Iter: InputConfig->AbilityInputActions)
+		{
+			if (Iter.InputAction && Iter.InputTag.IsValid())
+			{
+				if (PressedFunc)
+				{
+					BindAction(Iter.InputAction, ETriggerEvent::Started, Object, PressedFunc, Iter.InputTag);
+				}
+				if (ReleasedFunc)
+				{
+					BindAction(Iter.InputAction, ETriggerEvent::Completed, Object, ReleasedFunc, Iter.InputTag);
+				}
+				if (HeldFunc)
+				{
+					BindAction(Iter.InputAction, ETriggerEvent::Triggered, Object, HeldFunc, Iter.InputTag);
+				}
+			}
+		}
+	}
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
