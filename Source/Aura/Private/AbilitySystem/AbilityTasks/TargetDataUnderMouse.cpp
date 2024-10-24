1d37c913c5b23d895561b5af61cc5ed178c54fb3
JiangXin12777 <2649340476@qq.com>
2024-10-24 15:04:37 +0800

    feat: 11.4 创建自定义AbilityTasks(AT)(https://blog.51cto.com/u_15948039/10537166)

---
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -0,0 +1,26 @@
+﻿// 
+
+
+#include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+	
+	return MyObj;
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+	Super::Activate();
+
+	if (Ability && Ability->GetCurrentActorInfo())
+	{
+		if (APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get())
+		{
+			FHitResult HitRes;
+			PC->GetHitResultUnderCursor(ECC_Visibility, false, HitRes);
+			ValidData.Broadcast(HitRes.Location);
+		}
+	}
+}
