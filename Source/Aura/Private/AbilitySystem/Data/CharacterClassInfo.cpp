438bb45155eb2c63eba1f6006c65b217e7d37635
JiangXin12777 <2649340476@qq.com>
2024-10-30 16:43:49 +0800

    feat: 12.2 创建角色属性配置信息

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+﻿// 
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+const FCharacterClassDefaultInfo& UCharacterClassInfo::FindClassDefaultInfo(ECharacterClass CharacterClass) const
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
---
+++ Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,54 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	/** 元素师 */
+	ECC_Elementalist,
+
+	/** 战士 */
+	ECC_Warrior,
+
+	/** 游侠 */
+	ECC_Ranger,
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, Category = "Class Default")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	/** 不同角色的主要属性不同 */
+	UPROPERTY(EditDefaultsOnly, Category = "Characer Class Default")
+	TMap<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassInformation;
+
+	/** 次要属性(通用) */
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Default")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+
+	/** 重要属性(通用) */
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Default")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+
+	const FCharacterClassDefaultInfo& FindClassDefaultInfo(ECharacterClass CharacterClass) const;
+};
