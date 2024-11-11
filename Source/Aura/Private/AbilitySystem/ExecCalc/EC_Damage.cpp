b1700c471227e4cbb1928c6c9d115feec493cff2
JiangXin12777 <2649340476@qq.com>
2024-11-11 17:18:41 +0800

    feat: 13.12 创建伤害 ExecutionCalculation 计算类

---
+++ Source/Aura/Private/AbilitySystem/ExecCalc/EC_Damage.cpp
@@ -0,0 +1,22 @@
+
+
+
+#include "AbilitySystem/ExecCalc/EC_Damage.h"
+
+#include "AbilitySystemComponent.h"
+
+UEC_Damage::UEC_Damage()
+{
+}
+
+void UEC_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	const AActor* SourceActor = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	const AActor* TargetActor = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
+	const FGameplayEffectSpec& GESpec = ExecutionParams.GetOwningSpec();
+}
---
+++ Source/Aura/Public/AbilitySystem/ExecCalc/EC_Damage.h
@@ -0,0 +1,21 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "EC_Damage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UEC_Damage : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UEC_Damage();
+
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+};
