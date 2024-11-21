74813d961937840976201fbf07763cfc40da063b
JiangXin12777 <2649340476@qq.com>
2024-11-21 11:23:03 +0800

    feat: 13.14 计算阻挡几率

--- Source/Aura/Private/AbilitySystem/ExecCalc/EC_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/EC_Damage.cpp
@@ -1,15 +1,19 @@
 #include "AbilitySystem/ExecCalc/EC_Damage.h"
 
 #include "AbilitySystemComponent.h"
+#include "AuraGameplayTags.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 
+// 定义 Armor 变量以及 Armor 的属性捕获参数
 struct AuraDamageStatics
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 	
 	AuraDamageStatics()
 	{
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, BlockChance, Target, false);
 	}
 };
 
@@ -22,6 +26,7 @@ static const AuraDamageStatics& DamageStatics()
 UEC_Damage::UEC_Damage()
 {
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
 }
 
 void UEC_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -39,13 +44,29 @@ void UEC_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionPara
 	EvaluateParameters.SourceTags = GESpec.CapturedSourceTags.GetAggregatedTags();
 	EvaluateParameters.TargetTags = GESpec.CapturedTargetTags.GetAggregatedTags();
 	
-	float Armor = 0.f;
-	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, Armor);
-	Armor = FMath::Max<float>(0.f, Armor);
-
 	// TODO:: 计算...
-	Armor++;
+	
+	// float Armor = 0.f;
+	// 参数捕获
+	// ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, Armor);
+	// Armor = FMath::Max<float>(0.f, Armor);
+	// Armor++;
 
-	FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	// FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	// OutExecutionOutput.AddOutputModifier(EvaluatedData);
+
+	// Get damage set by caller Magnitude
+	float Damage = GESpec.GetSetByCallerMagnitude(FAuraGameplayTags::Get()->Damage);
+
+	// 捕获目标上的 BlockChance，并确定是否成功阻止 如果阻止，则将伤害减半。
+	float TargetBlockChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluateParameters, TargetBlockChance);
+	TargetBlockChance = FMath::Max(TargetBlockChance, 0.f);
+	// 几率测算
+	const bool bBlocked = FMath::RandRange(1, 100) < TargetBlockChance;
+	Damage = bBlocked ? Damage / 2.f : Damage;
+
+	// 提交修改
+	FGameplayModifierEvaluatedData EvaluatedData(UAuraAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
