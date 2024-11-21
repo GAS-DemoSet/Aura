c94df27bd8874c7bdd9848e74d9457d96280aacb
JiangXin12777 <2649340476@qq.com>
2024-11-21 10:04:18 +0800

    feat: 13.13 计算属性值

--- Source/Aura/Private/AbilitySystem/ExecCalc/EC_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/EC_Damage.cpp
@@ -1,12 +1,27 @@
-
-
-
 #include "AbilitySystem/ExecCalc/EC_Damage.h"
 
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+struct AuraDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	
+	AuraDamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+	}
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+	static AuraDamageStatics Ds;
+	return Ds;
+}
 
 UEC_Damage::UEC_Damage()
 {
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 }
 
 void UEC_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -19,4 +34,18 @@ void UEC_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionPara
 	const AActor* TargetActor = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
 
 	const FGameplayEffectSpec& GESpec = ExecutionParams.GetOwningSpec();
+
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = GESpec.CapturedSourceTags.GetAggregatedTags();
+	EvaluateParameters.TargetTags = GESpec.CapturedTargetTags.GetAggregatedTags();
+	
+	float Armor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, Armor);
+	Armor = FMath::Max<float>(0.f, Armor);
+
+	// TODO:: 计算...
+	Armor++;
+
+	FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
