d9526b18e70db9a599ae31c61b4099e4d3433531
JiangXin12777 <2649340476@qq.com>
2024-12-12 16:23:13 +0800

    feat: 15.4 黑板参数

--- Source/Aura/Private/AI/BTService/BTS_FindNearestPlayer.cpp
+++ Source/Aura/Private/AI/BTService/BTS_FindNearestPlayer.cpp
@@ -3,11 +3,21 @@
 
 #include "AI/BTService/BTS_FindNearestPlayer.h"
 #include "AIController.h"
+#include "Kismet/GameplayStatics.h"
 
 void UBTS_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 
-	GEngine->AddOnScreenDebugMessage(1, 1.f, FColor::Black, *AIOwner.GetName());
-	GEngine->AddOnScreenDebugMessage(2, 1.f, FColor::Blue, *ActorOwner.GetName());
+	// GEngine->AddOnScreenDebugMessage(1, 1.f, FColor::Black, *AIOwner.GetName());
+	// GEngine->AddOnScreenDebugMessage(2, 1.f, FColor::Blue, *ActorOwner.GetName());
+
+	APawn* OwningPawn = AIOwner->GetPawn();
+	if (OwningPawn != nullptr)
+	{
+		const FName TargetTag = OwningPawn->ActorHasTag(FName("Player")) ? FName("Enemy") : FName("Player");
+
+		TArray<AActor*> ActorsWithTag;
+		UGameplayStatics::GetAllActorsWithTag(OwningPawn, TargetTag, ActorsWithTag);
+	}
 }
